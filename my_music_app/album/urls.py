30f64be348d6b710614d47bf272b93c7956e78e8
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-05-18 18:37:56 +0200

    added logic for album views and profile views

--- my_music_app/album/urls.py
+++ my_music_app/album/urls.py
@@ -5,4 +5,6 @@ from my_music_app.album import views
 
 urlpatterns = [
     path('add/', views.add_album, name='add-album'),
+    path('details/<int:pk>', views.album_details, name='album-details'),
+    path('edit/<int:pk>', views.album_edit, name='album-edit')
 ]
--- my_music_app/album/views.py
+++ my_music_app/album/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 from my_music_app.album.forms import AlbumForm
+from my_music_app.album.models import Album
 from my_music_app.profile_app.models import Profile
 
 
@@ -18,3 +19,26 @@ def add_album(request):
         return redirect('home-page')
 
     return render(request=request, template_name='add-album.html', context=context)
+
+
+def album_details(request, pk):
+    album = Album.objects.get(id=pk)
+
+    context = {'album': album}
+    return render(request=request, template_name='album-details.html', context=context)
+
+
+def album_edit(request, pk):
+    album = Album.objects.get(id=pk)
+    form = AlbumForm(request.POST or None, instance=album)
+
+    if form.is_valid():
+        form.save()
+        return redirect('home-page')
+
+    context = {'album': album, 'form': form}
+    return render(request=request, template_name='edit-album.html', context=context)
+
+def album_delete(request, pk):
+    album = Album.objects.get(id=pk)
+    form = AlbumForm(request.POST or None, instance=album)
